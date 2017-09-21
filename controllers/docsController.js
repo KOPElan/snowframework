9afa8839880919ed94005ded2990b6641df9e785
Elan <dongxusheng@live.cn>
2017-09-21 16:43:28 +0800

    临时提交

--- controllers/docsController.js
+++ controllers/docsController.js
@@ -3,7 +3,11 @@ const response = require('../lib/context');
 const docsController = {
     indexAction: function (req, res) {
         response.view('/docs/index.html', res);
+    },
+    startAction: function (req, res) {
+        response.content('hello world',response.contentType.txt,res);
     }
 };
+
 
 module.exports = docsController;
