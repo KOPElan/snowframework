638640c811e0b30642a6fe0f1e0961b2e7e43732
dongxs <dongxs@hffss.com>
2017-11-29 13:54:50 +0800

    增加错误请求实例

--- test/controllers/homeController.js
+++ test/controllers/homeController.js
@@ -1,4 +1,4 @@
-var snowf=require('../../lib/snowf');
+var snowf = require('../../lib/snowf');
 
 const homeController = {
     indexAction: function (req, res) {
@@ -12,16 +12,19 @@ const homeController = {
                 content: 'hello vue example!',
                 alert: 'this is a model data'
             }
-        };      
+        };
         // 实现简单的filters
-        snowf.filters.allowGet(req,res);
-        
+        snowf.filters.allowGet(req, res);
+
         // 答复客户端
         res.send(snowf.actionResult.view, context);
     },
     aboutAction: function (req, res) {
         //TODO:业务逻辑                        
-        res.send(snowf.action.content(action.contentType.html),"hello content");        
+        res.send(snowf.actionResult.content(snowf.contentType.html), "hello content");
+    },
+    errorAction: function (req, res) {
+        snowf.response.badRequest(416, '错误请求实例', res);
     }
 }
 
