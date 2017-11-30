91e75c43176ab184afb2bd4685ad6d65d4eb7988
dongxs <dongxs@hffss.com>
2017-11-30 17:29:04 +0800

    增加了认证的功能

--- test/controllers/homeController.js
+++ test/controllers/homeController.js
@@ -1,4 +1,5 @@
 var snowf = require('../../lib/snowf');
+var url = require('url');
 
 const homeController = {
     indexAction: function (req, res) {
@@ -8,16 +9,35 @@ const homeController = {
                 topic: 'Hello Snowframework!',
                 content: 'A Simple nodeJS Web Framework, hello world !'
             },
-            viewModel: {
-                content: 'hello vue example!',
+            viewModel: {  
                 alert: 'this is a model data'
             }
         };
+
+        context.viewModel.isLogin = req.user != undefined;
+        context.viewModel.content = context.viewModel.isLogin ? 'You are already login：' + req.user.userName : 'You are already logout!';
+
+        //var q= url.parse(req.url,true);        
+
         // 实现简单的filters
         snowf.filters.allowGet(req, res);
 
         // 答复客户端
         res.send(snowf.actionResult.view, context);
+    },
+
+    loginAction: function (req, res) {
+        var userInfo = {
+            userName: 'dxsh126',
+            password: '123456'
+        };
+
+        snowf.signIn(res, userInfo, true);
+        res.send(snowf.actionResult.content('text/html'), '登陆成功');
+    },
+    logoutAction: function (req, res) {
+        snowf.signOut(res);
+        res.send(snowf.actionResult.content('text/html'), '登出成功');
     },
     aboutAction: function (req, res) {
         //TODO:业务逻辑                        
@@ -25,7 +45,7 @@ const homeController = {
     },
     errorAction: function (req, res) {
         snowf.response.badRequest(416, '错误请求实例', res);
-    }
+    },
 }
 
 module.exports = homeController;
